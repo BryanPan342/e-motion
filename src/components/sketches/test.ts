24c856c62741ae629950a5accb418f2db50aa2cf
Bryan Pan <bryanpan342@gmail.com>
2021-05-03 23:18:14 -0700

    add responsive behavior

--- src/components/sketches/test.ts
+++ src/components/sketches/test.ts
@@ -1,18 +1,16 @@
 import p5 from 'p5';
-import v from '../styles/_variables.scss';
 
 export default function sketch(p: p5): void {
   let canvas: p5.Renderer;
-  const margin = +v.textHeight.slice(0, -2);
 
   p.setup = () => {
-    canvas = p.createCanvas(window.innerWidth, window.innerHeight - margin);
+    canvas = p.createCanvas(window.innerWidth, window.innerHeight);
     canvas.id('p5-background');
     p.noStroke();
   };
 
   p.windowResized = () => {
-    p.resizeCanvas(window.innerWidth, window.innerHeight - margin, false);
+    p.resizeCanvas(window.innerWidth, window.innerHeight, false);
   };
 
   p.draw = () => {
