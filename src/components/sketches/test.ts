35b47435c83fe5fb12fe1566263b7d77aab402f3
Bryan Pan <bryanpan342@gmail.com>
2021-05-03 22:46:07 -0700

    prettier layout

--- src/components/sketches/test.ts
+++ src/components/sketches/test.ts
@@ -3,15 +3,16 @@ import v from '../styles/_variables.scss';
 
 export default function sketch(p: p5): void {
   let canvas: p5.Renderer;
+  const margin = +v.textHeight.slice(0, -2);
 
   p.setup = () => {
-    canvas = p.createCanvas(window.innerWidth, window.innerHeight - +v.textHeight.slice(0, -2));
+    canvas = p.createCanvas(window.innerWidth, window.innerHeight - margin);
     canvas.id('p5-background');
     p.noStroke();
   };
 
   p.windowResized = () => {
-    p.resizeCanvas(window.innerWidth, window.innerHeight - +v.textHeight.slice(0, -2), false);
+    p.resizeCanvas(window.innerWidth, window.innerHeight - margin, false);
   };
 
   p.draw = () => {
