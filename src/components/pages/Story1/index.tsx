2259a1df3720c57c1fe45984d388a18beab4316c
Ivan <ivanma819@gmail.com>
2021-05-26 22:35:31 -0700

    Add script and change filenames

--- src/components/pages/Story1/index.tsx
+++ src/components/pages/Story1/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useHistory } from 'react-router';
-import Scene4_color from "../../../assets/colorFirework.png";
-import Scene1_fg from '../../../assets/Scene1_neutral.png';
-import Scene4_fg from '../../../assets/Scene4_cheerful.png';
+import Scene1_fg from '../../../assets/scene1.png';
+import Scene5_fg from '../../../assets/scene5.png';
+// import Scene4_fgBW from '../../../assets/Scene5_cheerful.png';
 import Wireframe from '../../../assets/wireframe.png';
 import Layout, { SceneProps } from '../../shared/Layout';
 import Scene1_bg from '../../sketches/Scene1_neutral';
-import Scene4_bg from '../../sketches/Scene4_neutral';
+import Scene5_bg from '../../sketches/Scene4_neutral';
 import test from '../../sketches/test';
 import test2 from '../../sketches/test2';
 
@@ -14,20 +14,6 @@ function Story1(): JSX.Element {
   const history = useHistory();
 
   const content: SceneProps[] = [
-    {
-      sketch: Scene4_bg,
-      expo: [
-        {
-          text: 'forrest test',
-          duration: 3000,
-        },
-        {
-          text: 'so slow no wonder u in online school',
-          duration: 2000,
-        },
-      ],
-      image: Scene4_color,
-    },
     {
       sketch: Scene1_bg,
       expo: [
@@ -66,6 +52,50 @@ function Story1(): JSX.Element {
         },
       ],
       image: Wireframe,
+    },
+    {
+      sketch: test2,
+      expo: [
+        {
+          text: 'blah blah blah more text.... asjdfalskdfjlqiwkejflidsf',
+          duration: 3000,
+        },
+      ],
+      image: Wireframe,
+    },
+    {
+      sketch: Scene5_bg,
+      expo: [
+        {
+          text: 'After school Brian and I would watch a lot of TV together. ',
+          duration: 3000,
+        },
+        {
+          text: 'It was a safe space for me.',
+          duration: 3000,
+        },
+        {
+          text: 'He was the one person that I never had to ask to turn on subtitles. ',
+          duration: 3000,
+        },
+        {
+          text: 'There was no obligation to socialize, or interact, or anything else I struggled with.',
+          duration: 3000,
+        },
+        {
+          text: 'We’d just sit there. And be together. ',
+          duration: 3000,
+        },
+        {
+          text: 'Maybe it didn’t mean much to him, because he had so many friends. But it meant a lot to me [joy].',
+          duration: 3000,
+        },
+        {
+          text: 'Both of us are grown now. We’ve matured a lot. And we understand each other better. ',
+          duration: 3000,
+        },
+      ],
+      image: Scene5_fg,
     },
   ];
 
